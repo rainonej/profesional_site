baae663d56e1750f60baddc1bd03850cdc5a9770
Jordan Rainone <47578985+rainonej@users.noreply.github.com>
2026-03-17 20:35:15 -0400

    fix: set base path for GitHub Pages and prefix all internal links

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://rainonej.github.io',
+  base: '/profesional_site',
   integrations: [tailwind()],
 });
